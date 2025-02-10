1be8f0413fe1ab3862bbf867eb4929d83ba7cc81
JohnSan36 <joao0206.jo@gmail.com>
2025-02-10 16:54:57 -0300

    Subi a pasta
    Sem a venv

---
+++ Aulas_livres/main.py
@@ -0,0 +1,6 @@
+def make_hotdog(size, *toppings):
+
+    print("\nMaking a " + str(size) +
+    "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
